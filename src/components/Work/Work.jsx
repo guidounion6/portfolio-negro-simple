d261660ecbd16ef91c7c4b47cf3f81d0a2599890
guido_union6 <guido_union6@hotmail.com>
2024-06-27 08:27:52 -0300

    Esthetical changes

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -26,7 +26,7 @@ const Work = () => {
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
             <span className="text-2xl font-bold text-white tracking-wider"></span>
             <div className="pt-8 text-center">
-              <a href="">
+              <a href="https://deliveryappqdeli.netlify.app/">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                   Live
                 </button>
@@ -43,9 +43,9 @@ const Work = () => {
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
             <span className="text-2xl font-bold text-white tracking-wider"></span>
             <div className="pt-8 text-center">
-              <a href="/">
+              <a href="https://github.com/guidounion6/Proyecto-Individual-Countries">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                  Live
+                  Repo
                 </button>
               </a>
             </div>
