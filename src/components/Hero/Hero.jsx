2cc7fd4ba6e4abb4f321976292160077356c5d5f
guido_union6 <guido_union6@hotmail.com>
2024-06-27 09:13:06 -0300

    Image changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     return ( 
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] md:h-[70vh] mx-auto py-8 bg-black">
 
-            <div className="col-span-1 my-auto mx-auto w-[300px] h-auto lg:w-[400px]">
+            <div className="col-span-1 my-auto pr-10 mx-auto w-[500px] h-auto lg:w-[500px]" id="unico">
                 <img src={heroimage} alt="hero image"/>
             </div>
 
