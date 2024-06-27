7c1382fb9e26713fc247a9420efe9abb8955ed55
guido_union6 <guido_union6@hotmail.com>
2024-06-27 08:47:22 -0300

    last last p change

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
                     <div className="my-auto mx-6">
                         <h2 className="text-4xl font-bold mb-4 primary-color">About Me</h2>
                         <p className="text-base lg:text-lg">
-                            My name is Guido Contartese i am an Argentinian Fullstack Developer. I love making sites and creating aesthetical solutions for my clients
+                            My name is Guido Contartese i am an Argentinian Fullstack Developer. I love making sites and creating aesthetical, practical and scalable solutions for my clients
                             while sharing my experience with colleagues and partners. I have been doing this for +2 years learning by myself and as a freelancer and one year ago started to do it professionally acheiving a degree in Fullstack Development. 
                             My main techs are JavaScript, React, Next, and Redux for client-side views, as well as Node.JS, Express, and Sequelize for server-side operations, and, of course, version control with Git.
                             Additionally, my skills extend to managing relational databases with PostgreSQL, MySQL, and API consumption. I wish you the best and hope we could work togheter sometime, if so...contact me!! 👇👇 
